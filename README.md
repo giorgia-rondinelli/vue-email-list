e23d89a428892eadf915e1e4b176a706980e96d0
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-03-19 14:53:09 +0100

    [add] generazione 10 mail

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ Attraverso l’apposita API di Boolean
 https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 ## Svolgimento
-- 
+- salvo l'API in un data 
+- creo una funzione che mi restituirà la mail random generata dall'API
+- salvo la mail in un data che stamperò in pagina 
+- richiamo la funzione in mounted 
+ 
+
+
--- script.js
+++ script.js
@@ -3,15 +3,29 @@ const{createApp}=Vue
 createApp({
   data(){
     return{
+      email: '',
+      api:'https://flynn.boolean.careers/exercises/api/random/mail',
+      
 
     }
 
   },
   methods:{
+    getApi(){
+      
+        axios.get(this.api)
+        .then((res)=>{
+          
+          this.listaEmail.push(this.email= res.data.response)
+        })
+      
+    }
+
 
   },
   mounted(){
-    
+    this.getApi();
+
   }
 
 }).mount('#app')
